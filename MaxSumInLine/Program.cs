cee3f57ab3bcd1cba255f14f70a898e5b92fd954
Artem Vlasov <baneslayer55@gmail.com>
2020-06-23 12:29:44 +0300

    added cl attributes

--- MaxSumInLine/Program.cs
+++ MaxSumInLine/Program.cs
@@ -8,58 +8,66 @@ namespace MaxSumInLine
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            while (true)
             {
-                while (true)
+                string filePath;
+
+                if (args.Length > 0)
+                {
+                    filePath = args[0];
+                }
+                else
                 {
                     Console.WriteLine("Please, enter file's path:");
-                    string filePath = Console.ReadLine();
+                    filePath = Console.ReadLine();
+                }                
 
-                    using (FileLinesSource importedFile = new FileLinesSource(filePath))
+                using (FileLinesSource importedFile = new FileLinesSource(filePath))
+                {
+                    try
                     {
-                        try
-                        {
-                            NumLineWithMaxSum maxSum = new NumLineWithMaxSum(importedFile);
+                        NumLineWithMaxSum maxSum = new NumLineWithMaxSum(importedFile);
 
-                            if (maxSum.NumLineWithMaxSumValue > 0)
-                            {
-                                Console.WriteLine($"In your file line {maxSum.NumLineWithMaxSumValue} has max value!\n");
-                            }
-                            else
-                            {
-                                Console.WriteLine("There are no valid data in input file!\n");
-                            }
-
-                            if (maxSum.InvalidStrings.Count > 0)
-                            {
-                                Console.WriteLine("Invalid strings in your file:");
-                                foreach (string str in maxSum.InvalidStrings)
-                                {
-                                    Console.WriteLine(str);
-                                }
-                            }
-                            else if (maxSum.NumLineWithMaxSumValue == 0 && maxSum.InvalidStrings.Count == 0)
-                            {
-                                Console.WriteLine("Hey! Рath leads to empty file. Check it!");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Well done! All datas in your file are valid! Grats!");
-                            }
-                            Console.WriteLine("\n");
-
-                        }
-                        catch (Exception ex)
+                        if (maxSum.NumLineWithMaxSumValue > 0)
                         {
-                            Console.WriteLine(ex.Message);
+                            Console.WriteLine($"In your file line {maxSum.NumLineWithMaxSumValue} has max value!\n");
                         }
-                        finally
+                        else
                         {
-                            importedFile.Dispose();
+                            Console.WriteLine("There are no valid data in input file!\n");
                         }
+
+                        if (maxSum.InvalidStrings.Count > 0)
+                        {
+                            Console.WriteLine("Invalid strings in your file:");
+                            foreach (string str in maxSum.InvalidStrings)
+                            {
+                                Console.WriteLine(str);
+                            }
+                        }
+                        else if (maxSum.NumLineWithMaxSumValue == 0 && maxSum.InvalidStrings.Count == 0)
+                        {
+                            Console.WriteLine("Hey! Рath leads to empty file. Check it!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Well done! All datas in your file are valid! Grats!");
+                        }
+                        Console.WriteLine();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
                     }
                 }
-            }
+                if (args.Length > 0)
+                {
+                    Console.WriteLine("The program is finished.\nPlease, press any key to close the program.");
+                    Console.ReadLine();
+                    break;
+                }
+            }            
         }
     }
 }
